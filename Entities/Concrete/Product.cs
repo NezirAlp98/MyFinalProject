dd35709729cfc12660714b38e1531e56d0773dea
nezir alp arslan <nezirarslan1234@gmail.com>
2021-02-07 21:21:10 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Abstract;
+
+namespace Entities.Concrete
+{
+    public class Product:IEntity        // classlar varsayılan olarak internal'dır.
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+
+    }
+}
