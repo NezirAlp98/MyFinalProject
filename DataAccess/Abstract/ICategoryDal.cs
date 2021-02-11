c71cdd6f589b9c01281868d4737761d2426adb2d
nezir alp arslan <nezirarslan1234@gmail.com>
2021-02-11 21:38:18 +0300

    11 şubat sonu

--- DataAccess/Abstract/ICategoryDal.cs
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -5,8 +5,8 @@ using Entities.Concrete;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal:IEntityRepository<Product>
+    public interface ICategoryDal:IEntityRepository<Category>
     {
-        
+       
     }
 }
