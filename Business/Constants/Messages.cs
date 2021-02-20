8fdd9c02f6a1edcf81f8071bcc3a6c198ca2f857
nezir alp arslan <nezirarslan1234@gmail.com>
2021-02-20 18:10:02 +0300

    20 şubat sonu

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        //static olunca class new'lenemez.
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        internal static string MaintenanceTime="Sistem bakımda";
+        internal static string ProductsListed="Ürünler listelendi";
+    }
+}
