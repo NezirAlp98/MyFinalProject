3115a600f4f38f8c0829fe7ab9401e2b7df9f8ba
nezir alp arslan <nezirarslan1234@gmail.com>
2021-04-04 00:29:55 +0300

    4 nisan commit

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -75,7 +75,7 @@ namespace WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseCors(builder=>builder.WithOrigins("http://localhost:55115").AllowAnyHeader());
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
             app.UseHttpsRedirection();
 
             app.UseRouting();
