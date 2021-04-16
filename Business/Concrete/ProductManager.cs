e7776c7336eb9eb061a5febff208600095e46e1b
nezir alp arslan <nezirarslan1234@gmail.com>
2021-04-16 19:12:46 +0300

    16 nisan son commit

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -63,7 +63,7 @@ namespace Business.Concrete
         }
 
         //Claim
-        [SecuredOperation("product.add,admin")]
+        //[SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         [CacheRemoveAspect("IProductService.Get")]
 
